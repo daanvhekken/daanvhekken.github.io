f93bb2d0c485770ea282a2ce6835441782f2afa8
Daan <90179778+daanvhekken@users.noreply.github.com>
2021-09-07 14:08:18 +0200

    add bootstrap css and javascript

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Script to open and close sidebar
+function open_sidemenu() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function close_sidemenu() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
